50ee402ba2a4260f2855afd4afd7aa9c93ce7a12
Joe Hernandez <yoshi.jhii@gmail.com>
2020-08-02 12:21:18 -0500

    updates to files

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,14 @@
+// Set up MySQL connection.
 var mysql = require('mysql');
 
 var connection;
 if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
+    // Database is local
     connection = mysql.createConnection({
+        port: 3306,
         host: 'localhost',
         user: 'root',
         password: 'password',
@@ -12,6 +16,7 @@ if (process.env.JAWSDB_URL) {
     })
 };
 
+// Make connection.
 connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -20,4 +25,5 @@ connection.connect(function (err) {
   console.log("connected as id " + connection.threadId);
 });
 
+// Export connection for our ORM to use.
 module.exports = connection;
--- models/burger.js
+++ models/burger.js
@@ -1,21 +1,32 @@
+// Import orm.js into burger.js
 var orm = require("../config/orm.js");
-
+// The code that will call the ORM functions using burger specific input for the ORM.
 var burger = {
-    selectAll: function (cb) {
-        orm.selectAll(function (res) {
+    // Display all burgers in the db.
+    all: (cb) => {
+        orm.all("burgers", function (res) {
             cb(res);
         });
     },
-    insertOne: function (burger, cb) {
-        orm.insertOne(burger, function (res) {
+    // Add a new burger to the db.
+    create: (cols, vals, cb) => {
+        orm.create("burgers", cols, vals, function (res) {
             cb(res);
         });
     },
-    updateOne: function (id, cb) {
-        orm.updateOne([id], function (res) {
+    // Change the devoured status to true.
+    update: (objColVals, condition, cb) => {
+        orm.update("burgers", objColVals, condition, function (res) {
+            cb(res);
+        });
+    },
+    // Delete a burger from the db.
+    delete: (condition, cb) => {
+        orm.delete("burgers", condition, function (res) {
             cb(res);
         });
     }
-};
+}
 
+// Export at the end of the burger.js file.
 module.exports = burger;
